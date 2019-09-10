e4d0a5e9c8f860ed25258e4ab6a61dd2c0dab0d7
Paula Herbska <paula.herbska@ing.com>
2019-09-10 20:32:00 +0200

    Send value from createnew to allreports to list

--- app/src/main/java/com/example/angela/assap/CreateNew.java
+++ app/src/main/java/com/example/angela/assap/CreateNew.java
@@ -3,6 +3,7 @@ package com.example.angela.assap;
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,23 +12,41 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import java.util.*;
+import android.view.LayoutInflater;
+import android.text.TextUtils;
+import android.content.Context;
 
 public class CreateNew extends AppCompatActivity {
 
     private static final int PERMISSION_CODE = 1000;
-    private  static final int IMAGE_CAPTURE_CODE = 1001;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
 
     ImageButton cameraButton;
     ImageView photo1;
     Uri image_uri;
 
+
+    Database db;
+    Button add_data;
+    EditText editText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createnew);
+
 
         photo1 = findViewById(R.id.photo1);
         cameraButton = findViewById(R.id.cameraButton);
@@ -35,19 +54,17 @@ public class CreateNew extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     if (checkSelfPermission(Manifest.permission.CAMERA) ==
                             PackageManager.PERMISSION_DENIED ||
                             checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                            PackageManager.PERMISSION_DENIED){
+                                    PackageManager.PERMISSION_DENIED) {
                         String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
                         requestPermissions(permission, PERMISSION_CODE);
-                    }
-                    else{
+                    } else {
                         openCamera();
                     }
-                }
-                else{
+                } else {
 
                 }
             }
@@ -62,24 +79,73 @@ public class CreateNew extends AppCompatActivity {
                 startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
             }
 
-            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-                switch (requestCode){
-                    case PERMISSION_CODE:{
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+                switch (requestCode) {
+                    case PERMISSION_CODE: {
                         if (grantResults.length > 0 && grantResults[0] ==
-                                PackageManager.PERMISSION_GRANTED){
+                                PackageManager.PERMISSION_GRANTED) {
                             openCamera();
                         }
                     }
                 }
             }
 
-            protected  void onActivityResult(int requestCode, int resultCode, Intent data){
-                if (resultCode == RESULT_OK){
+            protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+                if (resultCode == RESULT_OK) {
                     photo1.setImageURI(image_uri);
                 }
             }
 
         });
 
+
+        add_data = findViewById(R.id.button3);
+        editText = findViewById(R.id.editText5);
+        db = new Database(this);
+
+
+
+        add_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newEntry = editText.getText().toString();
+
+                if(editText.length() != 0){
+
+                    AddData(newEntry);
+                    editText.setText("");
+                    Intent createNewActivity = new Intent(CreateNew.this, MainReports.class);
+                    startActivity(createNewActivity);
+                }else{
+
+                    Toast.makeText(CreateNew.this,"Add something",Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    public void AddData(String newEntry){
+        boolean insertData = db.addData(newEntry);
+
+        if(insertData==true){
+            Toast.makeText(CreateNew.this,"Data added",Toast.LENGTH_LONG).show();
+        }else{
+
+            Toast.makeText(CreateNew.this,"Data not added!!!",Toast.LENGTH_LONG).show();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/angela/assap/MainReports.java
+++ app/src/main/java/com/example/angela/assap/MainReports.java
@@ -1,20 +1,62 @@
 package com.example.angela.assap;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+import org.w3c.dom.Text;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class MainReports extends AppCompatActivity {
 
     ImageButton createNewButton;
+    Database db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.allreports);
+
+    ListView listView = (ListView) findViewById(R.id.mobile_list);
+    db = new Database(this);
+
+
+    ArrayList<String> theList = new ArrayList<>();
+    Cursor data = db.viewData();
+
+    if(data.getCount() == 0){
+
+        Toast.makeText(MainReports.this,"Database was empty",Toast.LENGTH_LONG).show();
+    }else{
+        while(data.moveToNext()){
+            theList.add(data.getString(1));
+            ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,theList);
+            listView.setAdapter(listAdapter);
+        }
+
+    }
+
+
 
         createNewButton = (ImageButton) findViewById(R.id.createnew);
         createNewButton.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +66,9 @@ public class MainReports extends AppCompatActivity {
                 startActivity(createNewActivity);
             }
         });
+
     }
+
 }
+
+
